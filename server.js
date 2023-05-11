46741b1a8738f36e820e94b016372a474587805e
Archisman Das <72722967+Arch0125@users.noreply.github.com>
2023-05-11 16:22:52 +0530

    add multichain support

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require('path')
 const socketIO = require('socket.io');
 const PORT = process.env.PORT || 8002
 const fetch = require('node-fetch');
+require('dotenv').config();
 
-// start the express server with the appropriate routes for our webhook and web requests
 var app = express()
   .use(express.static(path.join(__dirname, 'public')))
   .use(express.json())
@@ -12,29 +12,26 @@ var app = express()
   .get('/*', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
 
-// start the websocket server
 const io = socketIO(app);
 
-// listen for client connections/calls on the WebSocket server
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
   socket.on('register address', (msg) => {
-    //send address to Alchemy to add to notification
     addAddress(msg);
   });
 });
 
-// notification received from Alchemy from the webhook. Let the clients know.
 function notificationReceived(req) {
   console.log("notification received!");
+  const res = req.body;
+  console.log(res);
   io.emit('notification', JSON.stringify(req.body));
 }
 
-// add an address to a notification in Alchemy
 async function addAddress(new_address) {
   console.log("adding address " + new_address);
-  const body = { webhook_id: 'wh_3aintcu9frhs6igj', addresses_to_add: [new_address], addresses_to_remove: [] };
+  const body = { webhook_id: [process.env.ETH_MAINNET_WEBHOOK_ID], addresses_to_add: [new_address], addresses_to_remove: [] };
   try {
     fetch('https://dashboard.alchemyapi.io/api/update-webhook-addresses', {
       method: 'PATCH',
